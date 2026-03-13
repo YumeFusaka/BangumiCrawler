664ce6786b4e4c17a2c42fbe17e510df5b153e18
MonPanache <1336679971@qq.com>
2026-03-14 00:30:29 +0800

    去重优化

--- src/main/java/com/bangumi_crawler/service/serviceImpl/GalGameServiceImpl.java
+++ src/main/java/com/bangumi_crawler/service/serviceImpl/GalGameServiceImpl.java
@@ -9,26 +9,21 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class GalGameServiceImpl extends ServiceImpl<GalGameMapper, GalGame> implements IGalGameService {
 
     @Autowired
     private GalGameMapper gameMapper;
-
+    
     @Override
     public void saveData(GalGame game) {
-        QueryWrapper<GalGame> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("subject_id", game.getSubjectId());
-        List<GalGame> games = gameMapper.selectList(queryWrapper);
-        if (games.isEmpty())
-            gameMapper.insert(game);
-        else {
-            UpdateWrapper<GalGame> updateWrapper = new UpdateWrapper<>();
-            updateWrapper.eq("subject_id", game.getSubjectId());
-            gameMapper.update(game, updateWrapper);
-        }
-
+        if (game.getSubjectId() == null) return;
+        gameMapper.insertOrUpdate(game);
     }
 }
